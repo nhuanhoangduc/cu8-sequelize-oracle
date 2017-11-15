e3edb224b813b049a3c1c5b840a3a61b17fe9f84
Vagabondan <strannix.vagabondan@gmail.com>
2017-11-15 23:27:08 +0300

    Long object names in select fixed

--- index.js
+++ index.js
@@ -61,6 +61,7 @@ var Sequelize=require("./lib/sequelize")
     if(typeof sqlQuery === 'object' && sqlQuery.sql){
       sql=sqlQuery.sql;
       options.bind=sqlQuery.bind;
+      options.aliases = sqlQuery.aliases;
     }else{
       sql=sqlQuery;
     }
